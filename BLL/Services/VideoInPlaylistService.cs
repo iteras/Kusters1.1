711ed2f273f2db184c4e6a39ba91fc43dc93c1e9
Immu . <teras_8@hotmail.com>
2016-06-09 19:10:41 +0300

    service added, cors added, but new one is out already

--- BLL/Services/VideoInPlaylistService.cs
+++ BLL/Services/VideoInPlaylistService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using BLL.DTO;
 using BLL.Factories;
 using DAL;
+using DAL.Interfaces;
 using DAL.Repositories;
 using DAL.Repositories.Video;
 using Domain.Video;
@@ -13,9 +14,10 @@ using NLog.LayoutRenderers.Wrappers;
 
 namespace BLL.Services
 {
-   public class VideoInPlaylistService
+    public class VideoInPlaylistService
     {
         private DAL.Interfaces.IVideoInPlaylistRepository _repo;
+        private IPlaylistRepository _playlistRepo;
         private VideoInPlaylistFactory _factory;
         private VideoFactory _videoFactory;
         private PlaylistFactory _playlistFactory;
@@ -28,31 +30,48 @@ namespace BLL.Services
             this._videoFactory = new VideoFactory();
         }
 
-       public List<VideoInPlaylistDTO> GetAllVideoInPlaylists()
-       {
-           return _repo.All.Select(x => _factory.CreateBasicVideoInPlaylistDto(x)).ToList();
-       }
+        public List<VideoInPlaylistDTO> GetAllVideoInPlaylists()
+        {
+            return _repo.All.Select(x => _factory.CreateBasicVideoInPlaylistDto(x)).ToList();
+        }
 
         /*
          *get all videos in 1 playlist by playlist id 
          */
-       public List<VideoDTO> GetAllVideosInPlaylistByPlaylistId(int playlistId)
-       {
-           return
-               _repo.All.Where(x => x.PlaylistId == playlistId)
-                   .Select(x => _videoFactory.CreateBasicVideoDto(x.Video))
-                   .ToList();
-       }
+        public List<VideoDTO> GetAllVideosInPlaylistByPlaylistId(int playlistId)
+        {
+            return
+                _repo.All.Where(x => x.PlaylistId == playlistId)
+                    .Select(x => _videoFactory.CreateBasicVideoDto(x.Video))
+                    .ToList();
+        }
+
+        /*
+         * get all videos in playlist by playlist name
+         */
+        public List<VideoDTO> GetAllVideosInPlaylistByPlaylistTitle(string playlistTitle)
+        {
+            Playlist searchedPlaylist = _playlistRepo.All.FirstOrDefault(x => x.PlaylistName == playlistTitle); //get searched playlist
+            if (searchedPlaylist == null)
+            {
+                return null;
+            }
+            List<Video> vidosInSearchedPlaylist =
+                _repo.All.Where(x => x.PlaylistId == searchedPlaylist.PlaylistId).Select(x => x.Video).ToList();        //get all videoInPlaylists where their playlistId == searchedplaylist ID
+                                                                                                                        //AND get from that object Video object and put in list
+            return vidosInSearchedPlaylist.Select(x => _videoFactory.CreateBasicVideoDto(x)).ToList();                  //make found videos to VideoDTO
+        }
 
         /*
          * get all playlists for 1 video by video id
          */
-       public List<VideoDTO> GetAllPlaylistsForVideoByVideoId(int videoId)
-       {
-           return
-               _repo.All.Where(x => x.VideoId == videoId)
-                   .Select(x => _videoFactory.CreateBasicVideoDto(x.Video))
-                   .ToList();
-       } 
+        public List<VideoDTO> GetAllPlaylistsForVideoByVideoId(int videoId)
+        {
+            return
+                _repo.All.Where(x => x.VideoId == videoId)
+                    .Select(x => _videoFactory.CreateBasicVideoDto(x.Video))
+                    .ToList();
+        }
+
     }
 }
